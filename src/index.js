d67d954f060f9371f76e12231bd68051830be580
Evgeny1021 <zelenskii.evgeny@gmail.com>
2023-08-06 16:43:02 +0300

    init: reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n){
+  const nStr = n.toString();
+  let strLength = nStr.length;
+  result = ''
+  if (strLength >= 3) {
+    return result = (nStr.at(-1) + nStr.at(-2) + nStr.at(-3))
+  }
+  if (strLength == 2) {
+    return result (nStr.at(-1) + nStr.at(-2))
+  }
+  if (strLength == 1) {
+    return result (nStr.at(-1))
+  }
 }
